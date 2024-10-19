d72ea09344af91197be68148407dfa2a4e25a531
Santiago Martinez <s.martinezn@uniandes.edu.co>
2024-10-19 18:29:33 -0500

    Update config_setting

---
+++ configs/config_setting.py
@@ -0,0 +1,16 @@
+import torch
+
+# Configuration settings
+CONFIG = {
+    "base_dir": "data",
+    "image_folder": "ISIC-2017_Training_Data",
+    "gt_folder": "ISIC-2017_Training_Part1_GroundTruth",
+    "model_name": "unet",
+    "split_train": 0.8,
+    "split_val": 0.1,
+    "split_test": 0.1,
+    "image_size": 128,
+    "batch_size": 16,
+    "model_path": "results/",
+    "device": torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+}
