d01a15c82a42a4daef4d396d0909d5de0d03f8a2
Santiago Martinez <s.martinezn@uniandes.edu.co>
2024-10-20 22:47:45 -0500

    Update trial and error evaluation

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,6 @@
 from configs.config_setting import CONFIG
 from models.unet import UNet
-# from models.attention_unet import AttUNet
+from models.attention_unet import AttUNet
 from models.trans_unet import TransUNet
 from utils.preparation_tools import prepare_datasets
 from results.evaluator import Evaluator
@@ -19,7 +19,7 @@ split_val = CONFIG["split_val"]
 split_test = CONFIG["split_test"]
 image_size = CONFIG["image_size"]
 batch_size = CONFIG["batch_size"]
-model_path = CONFIG["model_path"]+"unet_128_aug.pth"
+model_path = CONFIG["model_path"]+"attunet_128_aug.pth"
 device = CONFIG["device"]
 
 CONFIG_FINAL = CONFIG.copy()
@@ -27,7 +27,7 @@ CONFIG_FINAL["image_size"] = 128
 
 train_dataset, val_dataset, test_dataset = prepare_datasets(CONFIG_FINAL, train_transform_type="train")
 
-model = DataParallel(UNet())
+model = DataParallel(AttUNet())
 model.load_state_dict(torch.load(model_path, map_location=device))
 model.to(device)
 
@@ -38,13 +38,13 @@ test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 evaluator = Evaluator(model_path, model, test_dataloader, device)
 
 # Evaluate the model
-avg_dice, avg_iou, avg_precision, avg_recall, max_indices, min_indices = evaluator.evaluate()
+avg_dice, avg_iou, avg_accuracy, avg_recall, max_indices, min_indices = evaluator.evaluate()
 
 # Print results
 print("\n----- Evaluation Results -----")
 print(f"Average Dice Coefficient: {avg_dice:.4f}")
 print(f"Average IoU Score: {avg_iou:.4f}")
-print(f"Average Precision: {avg_precision:.4f}")
+print(f"Average Accuracy: {avg_accuracy:.4f}")
 print(f"Average Recall: {avg_recall:.4f}")
 
 # Plot the image with the highest and lowest Dice score
@@ -61,11 +61,11 @@ plot_img_mask_pred(test_dataset, index=min_indices[0], plot_pred=True, model=mod
 # print("\nPlotting the image with the lowest IoU score:")
 # plot_img_mask_pred(test_dataset, index=min_indices[1], plot_pred=True, model=model, device=device)
 
-# # Plot the images with the highest and lowest precision scores
-# print("\nPlotting the image with the highest precision:")
+# # Plot the images with the highest and lowest accuracy scores
+# print("\nPlotting the image with the highest accuracy:")
 # plot_img_mask_pred(test_dataset, index=max_indices[2], plot_pred=True, model=model, device=device)
 
-# print("\nPlotting the image with the lowest precision:")
+# print("\nPlotting the image with the lowest accuracy:")
 # plot_img_mask_pred(test_dataset, index=min_indices[2], plot_pred=True, model=model, device=device)
 
 # # Plot the images with the highest and lowest recall scores
